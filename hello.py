792fbe314267f19744300a8b9039b1a7a2787891
JanayeRo23123 <janaye.rouser1@gmail.com>
2024-02-06 21:59:31 -0600

    108.1

---
+++ hello.py
@@ -0,0 +1,67 @@
+print ("hello world!")
+var = 10
+print (var)
+
+#variables
+x = 5
+y = 3
+
+#operations
+sum =  x + y
+print (sum)
+
+#string operations
+name = "adrian"
+gretting = "Hello" + name + "!"
+print (gretting)
+
+#list //this is the arrays
+numbers = [1, 2, 3, 4, 5]
+
+#loop 
+for number in numbers:
+    print (number)
+
+#loop 
+for number in numbers:
+    print (number)
+
+age = 15    
+if age > 21:
+    print ("youre an adult")    
+else:
+    print ("you're a minor")
+
+print("you're an adult") if age > 21 else print("you're a minor")
+#for(i=0; i<numbers; i++)
+#  let number = numbers[
+#      console.log(number)
+#JS comparative
+#fuction(){
+#console.log("fuction");    
+#}
+#Define the function in python
+def greet(name):
+    return "Hello " + name
+#Call the function
+result = greet("Adrian")
+print (result)
+example = 1
+example2 = "2"
+print (example + int(example2))
+#function to calculate the power of a number
+def power(base, exponent=2):
+    return base ** exponent
+#call the function
+result = power(2)
+result2 = power(3,3)
+#print result
+print (result)
+print (result2)
+##Create a fuction that multiply 2 parameters (numbers)
+def multiply (number1, number2):
+    multiplyResult = number1*number2
+    return multiplyResult
+#call the function
+product = multiply(12,12)
+print (product)
---
+++ test1.py
@@ -0,0 +1,37 @@
+
+#this is a comment... to figure out the comments for any language just press ctrl+/ and that will automatically enter the command for a comment in the language you're using
+
+# for python the function follows indentation blocks 
+def say_hello() :
+    print("Hello")
+    print("I'm a function")
+# this break in indentation will complete a function
+    print("Inside")
+
+
+
+def say_hi(name):
+    print("Hi " + name)
+
+
+
+def give_me_a_beer(age):
+    if age >= 90:
+        print("Enjoy! it's on the house!")
+    elif age >=21:
+        print("Enjoy!!")
+    else:
+        print("No can do captain!")
+
+
+# function calls
+say_hello()
+
+say_hi("Janaye")
+
+
+give_me_a_beer(31)
+give_me_a_beer(25)
+give_me_a_beer(19)
+
+
